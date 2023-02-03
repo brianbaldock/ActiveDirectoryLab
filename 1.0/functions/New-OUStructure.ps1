0a46a0e0387663ae26560523d66d35ac3831a753
Brian Baldock <72090857+brianbaldock@users.noreply.github.com>
2023-02-02 22:08:31 -0500

    New Functions
    Adding new functions for the module. Work in progress

--- 1.0/functions/New-OUStructure.ps1
+++ 1.0/functions/New-OUStructure.ps1
@@ -4,64 +4,64 @@
             Create the OU structure for the lab
         .DESCRIPTION
             Tests for existance of an OU Structure and generates a pre-formatted OU structure for the lab if none is found.
-        .PARAMETER CompanyName
+        .PARAMETER companyName
             The name of the company to be used for the OU Structure
-        .PARAMETER DomDN
+        .PARAMETER domainDN
             The DN of the domain to be used for the OU Structure
-        .PARAMETER OULayout
+        .PARAMETER ouLayout
             The OU Structure to be used for the lab, formatted as comma seperated string array
         .PARAMETER LogLocation
             The location to save the log file
         .EXAMPLE
-            New-OUStructure -CompanyName "Lab" -DomDN $DomDN -OULayout $OULayout -LogLocation $LogLocation -LogFile $LogFile
+            New-OUStructure -companyName "Lab" -domainDN $domainDN -ouLayout $ouLayout -LogLocation $LogLocation -logFile $logFile
         .NOTES
             Internal function
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $True)]
-        [String]$CompanyName,
-        [String]$DomDN,
-        [array]$OULayout,
-        [String]$LogLocation,
-        [String]$LogFile
+        [String] $companyName,
+        [String] $domainDN,
+        [array] $ouLayout,
+        [String] $logLocation,
+        [String] $logFile
     )
     process {
-        if(!(Get-ADOrganizationalUnit -Filter {Name -like $CompanyName})){
+        if(!(Get-ADOrganizationalUnit -Filter {Name -like $companyName})){
             try{
-                New-ADOrganizationalUnit -DisplayName "$($CompanyName)" -Name "$($CompanyName)" -Path $DomDN
-                foreach($OU in $OULayout){
+                New-ADOrganizationalUnit -DisplayName "$($companyName)" -Name "$($companyName)" -Path $domainDN
+                foreach($OU in $ouLayout){
                     if(!(Get-ADOrganizationalUnit -Filter {Name -like $OU})){
                         try{
-                            New-ADOrganizationalUnit -DisplayName "$($OU)" -Name "$($OU)" -Path "OU=$($CompanyName),$($DomDN)"
-                            if(!(Get-ADOrganizationalUnit -Filter {Name -like $OU})){
-                                Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - Error attempting to create the OU $($OU) under $($CompanyName)"
-                                Write-Verbose "Error attempting to create the $($OU) under $($CompanyName)"
+                            New-ADOrganizationalUnit -DisplayName "$($OU)" -Name "$($OU)" -Path "OU=$($companyName),$($domainDN)"
+                            if(Get-ADOrganizationalUnit -Filter {Name -like $OU}){
+                                Save-Output -logLocation $logLocation -logFile $logFile -InputString "$(Get-TimeStamp) - Created the OU $($OU) under OU=$($companyName),$($domainDN)"
+                                Write-Verbose "Succesfully created the OU $($OU) under OU=$($companyName),$($domainDN))"
                             }
                             else{
-                                Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - Created the OU $($OU) under OU=$($CompanyName),$($DomDN)"
-                                Write-Verbose "Succesfully created the OU $($OU) under OU=$($CompanyName),$($DomDN))"
+                                Save-Output -LogLocation $LogLocation -logFile $logFile -InputString "$(Get-TimeStamp) - Creation succeeded but unable to find the OU $($OU) under OU=$($companyName),$($domainDN)"
+                                Write-Verbose "Creation succeeded but unable to find the OU $($OU) under OU=$($companyName),$($domainDN)"
                             }
                         }
                         catch{
-                            Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - Error attempting to create the OU $($OU) under OU=$($CompanyName),$($DomDN)"
-                            Write-Verbose "Error attempting to create the OU $($OU) under OU=$($CompanyName),$($DomDN)"
+                            Save-Output -LogLocation $LogLocation -logFile $logFile -InputString "$(Get-TimeStamp) - Error attempting to create the OU $($OU) under OU=$($companyName),$($domainDN)"
+                            Write-Verbose "Error attempting to create the OU $($OU) under OU=$($companyName),$($domainDN)"
                         }
                     }
                     else{
-                        Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - OU=$($OU),$($CompanyName),$($DomDN) already exists."
-                        Write-Verbose "OU=$($OU),$($CompanyName),$($DomDN) already exists."
+                        Save-Output -LogLocation $LogLocation -logFile $logFile -InputString "$(Get-TimeStamp) - OU=$($OU),$($companyName),$($domainDN) already exists."
+                        Write-Verbose "OU=$($OU),$($companyName),$($domainDN) already exists."
                     }
                 }
             }
             catch{
-                Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - Error attempting to create a new root OU structure OU=$($CompanyName),$($DomDN)"
-                Write-Verbose "Error attempting to create a new root OU structure OU=$($CompanyName),$($DomDN)"
+                Save-Output -LogLocation $LogLocation -logFile $logFile -InputString "$(Get-TimeStamp) - Error attempting to create a new root OU structure OU=$($companyName),$($domainDN)"
+                Write-Verbose "Error attempting to create a new root OU structure OU=$($companyName),$($domainDN)"
             }
         }
         else{
-            Save-Output -LogLocation $LogLocation -LogFile $LogFile -InputString "$(Get-TimeStamp) - OU=$($CompanyName),$($DomDN) already exists."
-            Write-Verbose "OU=$($CompanyName),$($DomDN) already exists"
+            Save-Output -LogLocation $LogLocation -logFile $logFile -InputString "$(Get-TimeStamp) - OU=$($companyName),$($domainDN) already exists."
+            Write-Verbose "OU=$($companyName),$($domainDN) already exists"
         }
     }
 }
---
+++ 1.0/functions/New-RandomPassword.ps1
@@ -0,0 +1,20 @@
+﻿function New-RandomPassword {
+    <#
+        .SYNOPSIS
+            Generate a random password for a user
+        .DESCRIPTION
+            Generates a random password for the user and outputs the password. This function is borrowed from Armin Reiter: https://arminreiter.com/2021/07/3-ways-to-generate-passwords-in-powershell/
+        .EXAMPLE
+            New-RandomPassword -length 8 -amountOfNonAlphanumeric = 1
+        .NOTES
+            Internal function
+    #>
+    [CmdletBinding()]
+        param (
+            [Parameter(Mandatory)]
+            [int] $length,
+            [int] $amountOfNonAlphanumeric = 1
+        )
+        Add-Type -AssemblyName 'System.Web'
+        return [System.Web.Security.Membership]::GeneratePassword($Length, $AmountOfNonAlphanumeric)
+}
